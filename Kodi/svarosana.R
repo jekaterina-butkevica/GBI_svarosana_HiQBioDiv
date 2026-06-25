e56c679740307b2c2c3f19d418e8554f0633a6af
Jekaterīna Butkeviča <jb22035@edu.lu.lv>
2026-06-25 22:21:49 +0300

    start

---
+++ Kodi/svarosana.R
@@ -0,0 +1,19 @@
+# pakotnes
+
+
+
+# direktorijas
+bgi_mape <- "./BGI"
+
+
+GBI_kodi <- c("POLICA", "PHEARI", "CYASEM", "CUPMIN", "LYCPHL", "OCHSYL",
+              "MANJUR", "LASMEG", "COEPAM", "EUPAUR", "ANTCAR")
+
+tabula <- data.frame(
+  KODS = GBI_kodi
+)
+
+
+# TSS
+
+
